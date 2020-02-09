14d21336b41154998e33efa6f64f3579e955a157
Artur Gawron <artur@gawron.me>
2020-02-09 19:58:52 +0100

    Add eventListener on seats which are not occupied

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 const ticketPrice = +movieSelect.nodeValue;
+
+container.addEventListener('click', event => {
+    if (event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
+        console.log(event.target);
+    }
+});
