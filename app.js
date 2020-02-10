2af2fe772f40878e5eafca356cd3f73a44a4658c
Artur Gawron <artur@gawron.me>
2020-02-10 15:17:32 +0100

    Ass localStorage functionality
    Save in LocalStorage seats and price

--- app.js
+++ app.js
@@ -5,17 +5,48 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
+//SAVE SELECTED MOVIE INDEX & PRICE
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//UPDATE TOTAL & COUNT
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     const selectedSeatsCount = selectedSeats.length;
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 };
 
+//GET DATA FORM LOCALSTORAGE & POPULATE UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 //MOVIE CHOSE EVENT
 movieSelect.addEventListener('change', event => {
     ticketPrice = event.target.value;
+    setMovieData(event.target.selectedIndex, event.target.value);
     updateSelectedCount();
 })
 
@@ -26,3 +57,5 @@ container.addEventListener('click', event => {
         updateSelectedCount();
     }
 });
+
+updateSelectedCount();
