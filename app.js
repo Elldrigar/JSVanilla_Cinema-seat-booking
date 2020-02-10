6c05d33dd0becb0fcec40d511be24d8341515f7b
Artur Gawron <artur@gawron.me>
2020-02-10 14:33:37 +0100

    Add updateSelectedCount function
    Updating count of clicked seats and price

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-const ticketPrice = +movieSelect.nodeValue;
+const ticketPrice = +movieSelect.value;
 
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+
+
+}
 container.addEventListener('click', event => {
     if (event.target.classList.contains('seat') && !event.target.classList.contains('occupied')) {
         event.target.classList.toggle('selected');
+        updateSelectedCount();
     }
 });
